76e4ba2499726afeb08be487443821251d2c2835
Matthew Weier O'Phinney <matthew@zend.com>
2013-12-04 12:00:11 -0600

    zendframework/zf2@250c93b69eafddfda1aba03e6828ec2e1da92ad7 (1386112334)

--- Header/SetCookie.php
+++ Header/SetCookie.php
@@ -432,8 +432,6 @@ class SetCookie implements MultipleHeaderInterface
     }
 
     /**
-     * Set whether the value for this cookie should be quoted
-     *
      * @param  bool $quotedValue
      * @return SetCookie
      */
@@ -501,7 +499,7 @@ class SetCookie implements MultipleHeaderInterface
     }
 
     /**
-     * Check whether the value for this cookie should be quoted
+     * Check whether the cookie is a session cookie (has no expiry time set)
      *
      * @return bool
      */
@@ -531,11 +529,10 @@ class SetCookie implements MultipleHeaderInterface
     /**
      * Checks whether the cookie should be sent or not in a specific scenario
      *
-     * @param string|\Zend\Uri\Uri $uri URI to check against (secure, domain, path)
+     * @param string|Zend\Uri\Uri $uri URI to check against (secure, domain, path)
      * @param bool $matchSessionCookies Whether to send session cookies
      * @param int $now Override the current time when checking for expiry time
      * @return bool
-     * @throws Exception\InvalidArgumentException If URI does not have HTTP or HTTPS scheme.
      */
     public function match($uri, $matchSessionCookies = true, $now = null)
     {
@@ -579,6 +576,14 @@ class SetCookie implements MultipleHeaderInterface
      */
     public static function matchCookieDomain($cookieDomain, $host)
     {
+        if (! $cookieDomain) {
+            throw new Exception\InvalidArgumentException('$cookieDomain is expected to be a cookie domain');
+        }
+
+        if (! $host) {
+            throw new Exception\InvalidArgumentException('$host is expected to be a host name');
+        }
+
         $cookieDomain = strtolower($cookieDomain);
         $host = strtolower($host);
         // Check for either exact match or suffix match
@@ -597,6 +602,14 @@ class SetCookie implements MultipleHeaderInterface
      */
     public static function matchCookiePath($cookiePath, $path)
     {
+        if (! $cookiePath) {
+            throw new Exception\InvalidArgumentException('$cookiePath is expected to be a cookie path');
+        }
+
+        if (! $path) {
+            throw new Exception\InvalidArgumentException('$path is expected to be a host name');
+        }
+
         return (strpos($path, $cookiePath) === 0);
     }
 
