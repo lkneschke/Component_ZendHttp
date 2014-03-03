c1b8ef731d2a913eb81ee699519d9b9154073009
Matthew Weier O'Phinney <matthew@zend.com>
2014-03-03 17:00:33 -0600

    zendframework/zf2@844d43f4daca71b0a858b75ae1e5a22400048cac (1393886243)

--- Header/SetCookie.php
+++ Header/SetCookie.php
@@ -105,7 +105,7 @@ class SetCookie implements MultipleHeaderInterface
                 $keyValuePairs = preg_split('#;\s*#', $headerLine);
 
                 foreach ($keyValuePairs as $keyValue) {
-                    if (preg_match('#^(?P<headerKey>[^=]+)=\s*("?)(?P<headerValue>[^"]+)\2#', $keyValue, $matches)) {
+                    if (preg_match('#^(?P<headerKey>[^=]+)=\s*("?)(?P<headerValue>[^"]*)\2#', $keyValue, $matches)) {
                         $headerKey  = $matches['headerKey'];
                         $headerValue= $matches['headerValue'];
                     } else {
@@ -428,8 +428,6 @@ class SetCookie implements MultipleHeaderInterface
     }
 
     /**
-     * Set whether the value for this cookie should be quoted
-     *
      * @param  bool $quotedValue
      * @return SetCookie
      */
@@ -497,7 +495,7 @@ class SetCookie implements MultipleHeaderInterface
     }
 
     /**
-     * Check whether the value for this cookie should be quoted
+     * Check whether the cookie is a session cookie (has no expiry time set)
      *
      * @return bool
      */
@@ -527,11 +525,10 @@ class SetCookie implements MultipleHeaderInterface
     /**
      * Checks whether the cookie should be sent or not in a specific scenario
      *
-     * @param string|\Zend\Uri\Uri $uri URI to check against (secure, domain, path)
+     * @param string|Zend\Uri\Uri $uri URI to check against (secure, domain, path)
      * @param bool $matchSessionCookies Whether to send session cookies
      * @param int $now Override the current time when checking for expiry time
      * @return bool
-     * @throws Exception\InvalidArgumentException If URI does not have HTTP or HTTPS scheme.
      */
     public function match($uri, $matchSessionCookies = true, $now = null)
     {
@@ -575,6 +572,14 @@ class SetCookie implements MultipleHeaderInterface
      */
     public static function matchCookieDomain($cookieDomain, $host)
     {
+        if (! $cookieDomain) {
+            throw new Exception\InvalidArgumentException('$cookieDomain is expected to be a cookie domain');
+        }
+
+        if (! $host) {
+            throw new Exception\InvalidArgumentException('$host is expected to be a host name');
+        }
+
         $cookieDomain = strtolower($cookieDomain);
         $host = strtolower($host);
         // Check for either exact match or suffix match
@@ -593,6 +598,14 @@ class SetCookie implements MultipleHeaderInterface
      */
     public static function matchCookiePath($cookiePath, $path)
     {
+        if (! $cookiePath) {
+            throw new Exception\InvalidArgumentException('$cookiePath is expected to be a cookie path');
+        }
+
+        if (! $path) {
+            throw new Exception\InvalidArgumentException('$path is expected to be a host name');
+        }
+
         return (strpos($path, $cookiePath) === 0);
     }
 
